97fe8e0f7aab662ea7f8e5c31a6f2bae58804290
DenDen047 <sh.mn.nat@gmail.com>
2020-03-15 09:39:45 +0900

    run the simplest docker

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+cd docker
+
 # Start docker-compose
-cd docker && \
 docker-compose up -d --build
 
 # show the status of `app`
-cd docker && \
 docker-compose logs \
     --follow \
     --timestamps \
--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 cd docker && docker-compose down
-cd docker_notebook && docker-compose down
